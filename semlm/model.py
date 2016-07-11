19c6e4d9f2c19b86cf0ae509112be0b41daa02f8
Ben Lambert <ben@benjaminlambert.com>
2016-07-11 01:53:36 -0400

    Whitespace cleanup.

--- semlm/model.py
+++ semlm/model.py
@@ -1,12 +1,7 @@
-
-
-
 from semlm.features import sent_to_fv
-
 
 class lm():
     pass
-
 
 # The way I'm going to use this, the scores will be added
 # to a baseline LM score (coming from the lattice/nbest)
